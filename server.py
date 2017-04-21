c4ab924d17969c28f2d4a3d3a504933583f548f6
a31859 <delioamaral13@gmail.com>
2017-04-21 13:21:21 +0100

    add more documentation

--- server.py
+++ server.py
@@ -15,10 +15,17 @@ filename = None
 json_manager = JSONManager()
 
 def root_dir():
+    """Get the root directory of the current file"""
     return os.path.abspath(os.path.dirname(__file__))
 
 
 def get_file(filename):
+    """Get the inputed file
+        
+    filename -- the file to get
+    
+    @return file, None if it does not exist
+    """
     src = os.path.join(root_dir(), filename)
     if os.path.exists(src):
         return open(src).read()
@@ -26,12 +33,22 @@ def get_file(filename):
 
 
 def get_audio_response(text):
+    """Generate the audio response file and get the filename
+        
+    text -- text to be converted to speech
+    """
     global filename
     filename = None
     filename = tts.get_audio(text)
 
 
 def get_content_data(content):
+    """Get data from the json object
+        
+    content -- the json object
+    
+    @return data, audio values
+    """
     data = content['data']
     audio = content['speak']
     return data, audio
@@ -40,11 +57,19 @@ def get_content_data(content):
 @app.route('/')
 @app.route('/home')
 def index():
+    """Get the home page
+    
+    @return home page    
+    """
     return app.send_static_file('index.html')
 
 
 @app.route('/data', methods=['POST'])
 def add_data():
+    """Add new data
+    
+    @return json with the result    
+    """
     content = request.get_json(silent=True)
     if content is not None:
         data, audio = get_content_data(content)
@@ -59,6 +84,12 @@ def add_data():
 
 @app.route('/data/<int:data_id>', methods=['GET'])
 def get_data(data_id):
+    """Get data with inputed id
+    
+    data_id -- data id
+
+    @return json, with the result    
+    """
     speak = True if request.args.get('speak') == 'true' else False
     result = json_manager.get(data_id)
     if result is not None:
@@ -72,6 +103,12 @@ def get_data(data_id):
 
 @app.route('/data/<int:data_id>', methods=['DELETE'])
 def delete_data(data_id):
+    """Delete data with inputed id
+
+    data_id -- data id
+
+    @return json, with the result    
+    """
     speak = True if request.args.get('speak') == 'true' else False
     result = json_manager.delete(data_id)
     if result:
@@ -85,6 +122,12 @@ def delete_data(data_id):
 
 @app.route('/data/<int:data_id>', methods=['PUT'])
 def put_data(data_id):
+    """Update data with id inputed
+
+    data_id -- data id
+
+    @return json, with the result    
+    """
     content = request.get_json(silent=True)
     if content is not None:
         data, audio = get_content_data(content)
@@ -115,6 +158,10 @@ def capitalize():
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
+    """Do a search
+
+    @return json, with the result    
+    """
     if request.method == 'GET':
         query = request.args.get('q')
         audio = True if request.args.get('speak') == 'true' else False
@@ -138,12 +185,20 @@ def search():
 
 @app.route('/voicelist', methods=['GET'])
 def voice_list():
+    """Get the list of available voices
+    
+    @return json, with the result    
+    """
     voicelist = tts.get_voice_list()
     return jsonify(data=voicelist), 200
 
 
 @app.route('/voice', methods=['POST'])
 def set_voice():
+    """Set the voice
+
+    @return json, with the result    
+    """
     content = request.get_json(silent=True)
     if content is not None:
         data = content['data']
@@ -152,16 +207,25 @@ def set_voice():
         res = tts.update_settings(language_code=language, female=gender)
         get_audio_response(res)
         return jsonify(data=res), 200
+    abort(400) 
 
 
 @app.route('/voice', methods=['GET'])
 def get_voice():
+    """Get the current voice settings
+
+    @return json, with the result    
+    """
     code, gender = tts.get_voice_settings()
     return jsonify(data={'language':code, 'gender':gender}), 200
 
 
 @app.route('/audio_response')
 def audio():
+    """Get the audio file
+
+    @return audio file or 404 if no audio available    
+    """
     # will send the saved file if exists
     global filename
     if filename is not None:
@@ -173,6 +237,10 @@ def audio():
 
 @app.route('/get-audio', methods=['GET', 'POST'])
 def speak_input():
+    """Get the audio file
+
+    @return audio file    
+    """
     query = None
     content = None
     if request.method == 'GET':
